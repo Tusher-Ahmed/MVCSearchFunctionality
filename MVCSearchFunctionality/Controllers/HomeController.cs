ca804298611fadd67751db27b206d61924182e82
Tusher Ahmed <ahmedshad1997@gmail.com>
2023-09-17 00:09:16 +0600

    Add project files.

---
+++ MVCSearchFunctionality/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using MVCSearchFunctionality.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSearchFunctionality.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        ApplicationDbContext _db=new ApplicationDbContext();
+        public ActionResult Index(string searchBy,string search)
+        {
+            if (searchBy == "Name")
+            {
+                var data1=_db.Students.Where(u=>u.Name == search).ToList();
+                return View(data1);
+            }
+            if (searchBy == "Gender")
+            {
+                var data2 = _db.Students.Where(u => u.Gender == search).ToList();
+                return View(data2);
+            }
+            var data=_db.Students.ToList();
+            return View(data);
+        }
+    }
+}
---
+++ MVCSearchFunctionality/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using MVCSearchFunctionality.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCSearchFunctionality.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
